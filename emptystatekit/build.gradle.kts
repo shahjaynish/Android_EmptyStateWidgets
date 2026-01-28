916c7afeed129960d8abe77405993388ec088e36
shahjaynish <shahjaynnish1225@gmail.com>
2026-01-28 17:58:02 +0530

    Added publishing code

--- emptystatekit/build.gradle.kts
+++ emptystatekit/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -28,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    publishing {
+        singleVariant("release")
+    }
 }
 
 dependencies {
@@ -37,4 +41,24 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+
+                from(components["release"])
+
+                // CHANGE THIS TO YOUR USERNAME
+                groupId = "com.github.Excelsior-Technologies-Community"
+
+                // THIS CAN BE YOUR LIBRARY NAME
+                artifactId = "emptystatekit"
+
+                // This can be anything for now
+                version = "1.0.0"
+            }
+        }
+    }
 }
